cb96b433679564557166ca4f0952f5819afb683e
Belzee10 <ivan.perez@pri.jovenclub.cu>
2018-10-25 02:07:50 -0400

    added concurrently

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 server.listen(port);
 
-console.log('Server ready and listen to the port: ' + port);
+console.log("Server ready and listen to the port: " + port);
