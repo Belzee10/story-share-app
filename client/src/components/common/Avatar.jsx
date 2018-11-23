2ff3e72ff183dc3833f16648e9c8e6f64f116c4d
Belzee10 <ivan.perez@pri.jovenclub.cu>
2018-11-23 16:27:28 -0500

    created Avatar component

---
+++ client/src/components/common/Avatar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Avatar = props => {
+  const { image, alt, dimensions } = props;
+  return (
+    <div className="avatar">
+      <img
+        src={image}
+        alt={alt}
+        className="rounded-circle"
+        width={dimensions.width}
+        height={dimensions.height}
+      />
+    </div>
+  );
+};
+
+Avatar.propTypes = {
+  image: PropTypes.string,
+  alt: PropTypes.string.isRequired,
+  dimensions: PropTypes.object.isRequired
+};
+
+export default Avatar;
